9484bc953bef447ffd157b42b935ad859a731b88
Jarryd Geyer <jarrydgeyer@gmail.com>
2022-03-20 20:39:44 +0200

    Initial commit

---
+++ src/main/java/jadgetech/automotive/Automotive.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jadgetech.automotive;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author jarry
+ */
+public class Automotive 
+{
+    public static void main(String[] args) 
+    {
+        Locale.setDefault(new Locale("af", "ZA"));
+        new Menu().setVisible(true);
+    }
+}
